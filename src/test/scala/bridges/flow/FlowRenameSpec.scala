ad3c7b5f88622e9541d586abc9842e6cbad6ee15
Dave Gurnell <dave@untyped.com>
2019-02-23 11:17:16 +0000

    Tests and instances for Rename[A] in Typescript

---
+++ src/test/scala/bridges/flow/FlowRenameSpec.scala
@@ -0,0 +1,19 @@
+package bridges.flow
+
+import bridges.SampleTypes._
+import bridges.flow.FlowType._
+import bridges.flow.syntax._
+import org.scalatest._
+
+class FlowRenameSpec extends FreeSpec with Matchers {
+  "decl" in {
+    val actual = decl[Color].rename("red", "r")
+    val expected = "Color" := struct(
+        "r" := Intr,
+        "green" := Intr,
+        "blue" := Intr
+      )
+
+    actual shouldBe expected
+  }
+}
